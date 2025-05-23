71162bb823a0afeeb398275279bd01d5c0e03596
Bart <bartolomej.kozorog@gmail.com>
2025-05-23 15:18:07 +0200

    refactor reddit source to use go-reddit lib

--- go.mod
+++ go.mod
@@ -18,9 +18,12 @@ require (
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
 	github.com/go-shiori/dom v0.0.0-20230515143342-73569d674e1c // indirect
 	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/mattn/go-mastodon v0.0.9 // indirect
 	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+	github.com/vartanbeno/go-reddit/v2 v2.0.1 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
 )
 
 require (
--- pkg/sources/source-reddit.go
+++ pkg/sources/source-reddit.go
@@ -6,18 +6,16 @@ import (
 	"fmt"
 	"html"
 	"log/slog"
-	"net/http"
-	"net/url"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/go-shiori/go-readability"
+	"github.com/vartanbeno/go-reddit/v2/reddit"
 )
 
 type redditSource struct {
 	sourceBase          `yaml:",inline"`
-	Posts               forumPostList     `yaml:"-"`
+	Posts               []*redditPost     `yaml:"-"`
 	Subreddit           string            `yaml:"subreddit"`
 	Proxy               proxyOptionsField `yaml:"proxy"`
 	Style               string            `yaml:"style"`
@@ -30,16 +28,53 @@ type redditSource struct {
 	CommentsURLTemplate string            `yaml:"comments-url-template"`
 	Limit               int               `yaml:"limit"`
 	RequestURLTemplate  string            `yaml:"request-url-template"`
-
-	AppAuth struct {
+	client              *reddit.Client
+	AppAuth             struct {
 		Name   string `yaml:"name"`
 		ID     string `yaml:"ID"`
 		Secret string `yaml:"secret"`
-
-		enabled        bool
-		accessToken    string
-		tokenExpiresAt time.Time
 	} `yaml:"app-auth"`
+}
+
+type redditPost struct {
+	raw *reddit.Post
+}
+
+func (p *redditPost) UID() string {
+	return p.raw.ID
+}
+
+func (p *redditPost) Title() string {
+	return html.UnescapeString(p.raw.Title)
+}
+
+func (p *redditPost) Body() string {
+	body := p.raw.Body
+	if p.raw.URL != "" && !p.raw.IsSelfPost {
+		article, err := readability.FromURL(p.raw.URL, 5*time.Second)
+		if err == nil {
+			body += fmt.Sprintf("\n\nReferenced article: \n%s", article.TextContent)
+		} else {
+			slog.Error("Failed to fetch reddit article", "error", err, "url", p.raw.URL)
+		}
+	}
+	return body
+}
+
+func (p *redditPost) URL() string {
+	// TODO(pulse): Test format
+	return "https://www.reddit.com" + p.raw.Permalink
+}
+
+func (p *redditPost) ImageURL() string {
+	// TODO(pulse): Fetch thumbnail URL
+	// The go-reddit library doesn't provide direct access to thumbnail URLs
+	// We'll need to fetch this information separately if needed
+	return ""
+}
+
+func (p *redditPost) CreatedAt() time.Time {
+	return p.raw.Created.Time
 }
 
 func (s *redditSource) Feed() []Activity {
@@ -75,13 +110,23 @@ func (s *redditSource) Initialize() error {
 		}
 	}
 
-	a := &s.AppAuth
-	if a.Name != "" || a.ID != "" || a.Secret != "" {
-		if a.Name == "" || a.ID == "" || a.Secret == "" {
-			return errors.New("application name, client ID and client secret are required")
-		}
-		a.enabled = true
+	var client *reddit.Client
+	var err error
+
+	if s.AppAuth.ID != "" && s.AppAuth.Secret != "" {
+		client, err = reddit.NewClient(reddit.Credentials{
+			ID:     s.AppAuth.ID,
+			Secret: s.AppAuth.Secret,
+		})
+	} else {
+		client, err = reddit.NewReadonlyClient()
 	}
+
+	if err != nil {
+		return fmt.Errorf("creating reddit client: %v", err)
+	}
+
+	s.client = client
 
 	s.
 		withTitle("r/" + s.Subreddit).
@@ -92,7 +137,7 @@ func (s *redditSource) Initialize() error {
 }
 
 func (s *redditSource) Update(ctx context.Context) {
-	posts, err := s.fetchSubredditPosts()
+	posts, err := s.fetchSubredditPosts(ctx)
 	if !s.canContinueUpdateAfterHandlingErr(err) {
 		return
 	}
@@ -101,212 +146,62 @@ func (s *redditSource) Update(ctx context.Context) {
 		posts = posts[:s.Limit]
 	}
 
-	if s.ExtraSortBy == "engagement" {
-		posts.calculateEngagement()
-		posts.sortByEngagement()
-	}
-
 	s.Posts = posts
 }
 
-type subredditResponseJson struct {
-	Data struct {
-		Children []struct {
-			Data struct {
-				Id            string  `json:"ID"`
-				Title         string  `json:"title"`
-				SelfText      string  `json:"selftext"`
-				Upvotes       int     `json:"ups"`
-				Url           string  `json:"url"`
-				Time          float64 `json:"created"`
-				CommentsCount int     `json:"num_comments"`
-				Domain        string  `json:"domain"`
-				Permalink     string  `json:"permalink"`
-				Stickied      bool    `json:"stickied"`
-				Pinned        bool    `json:"pinned"`
-				IsSelf        bool    `json:"is_self"`
-				Thumbnail     string  `json:"thumbnail"`
-				Flair         string  `json:"link_flair_text"`
-				ParentList    []struct {
-					Id        string `json:"ID"`
-					Subreddit string `json:"subreddit"`
-					Permalink string `json:"permalink"`
-				} `json:"crosspost_parent_list"`
-			} `json:"data"`
-		} `json:"children"`
-	} `json:"data"`
-}
+func (s *redditSource) fetchSubredditPosts(ctx context.Context) ([]*redditPost, error) {
+	var posts []*reddit.Post
+	var err error
 
-func (s *redditSource) parseCustomCommentsURL(subreddit, postId, postPath string) string {
-	template := strings.ReplaceAll(s.CommentsURLTemplate, "{SUBREDDIT}", subreddit)
-	template = strings.ReplaceAll(template, "{POST-ID}", postId)
-	template = strings.ReplaceAll(template, "{POST-PATH}", strings.TrimLeft(postPath, "/"))
-
-	return template
-}
-
-func (s *redditSource) fetchSubredditPosts() (forumPostList, error) {
-	var client requestDoer = defaultHTTPClient
-	var baseURL string
-	var requestURL string
-	var headers http.Header
-	query := url.Values{}
-	app := &s.AppAuth
-
-	if !app.enabled {
-		baseURL = "https://www.reddit.com"
-		headers = http.Header{
-			"User-Agent": []string{getBrowserUserAgentHeader()},
-		}
-	} else {
-		baseURL = "https://oauth.reddit.com"
-
-		if app.accessToken == "" || time.Now().Add(time.Minute).After(app.tokenExpiresAt) {
-			if err := s.fetchNewAppAccessToken(); err != nil {
-				return nil, fmt.Errorf("fetching new app access token: %v", err)
-			}
-		}
-
-		headers = http.Header{
-			"Authorization": []string{"Bearer " + app.accessToken},
-			"User-Agent":    []string{app.Name + "/1.0"},
-		}
-	}
-
-	if s.Limit > 25 {
-		query.Set("limit", strconv.Itoa(s.Limit))
+	opts := &reddit.ListOptions{
+		Limit: s.Limit,
 	}
 
 	if s.Search != "" {
-		query.Set("q", s.Search+" subreddit:"+s.Subreddit)
-		query.Set("sort", s.SortBy)
-		requestURL = fmt.Sprintf("%s/search.json?%s", baseURL, query.Encode())
-	} else {
-		if s.SortBy == "top" {
-			query.Set("t", s.TopPeriod)
+		searchOpts := &reddit.ListPostSearchOptions{
+			ListPostOptions: reddit.ListPostOptions{
+				ListOptions: reddit.ListOptions{
+					Limit: s.Limit,
+				},
+			},
+			Sort: s.SortBy,
 		}
-		requestURL = fmt.Sprintf("%s/r/%s/%s.json?%s", baseURL, s.Subreddit, s.SortBy, query.Encode())
+		posts, _, err = s.client.Subreddit.SearchPosts(ctx, s.Subreddit, s.Search, searchOpts)
+	} else {
+		switch s.SortBy {
+		case "hot":
+			posts, _, err = s.client.Subreddit.HotPosts(ctx, s.Subreddit, opts)
+		case "new":
+			posts, _, err = s.client.Subreddit.NewPosts(ctx, s.Subreddit, opts)
+		case "top":
+			topOpts := &reddit.ListPostOptions{
+				ListOptions: reddit.ListOptions{
+					Limit: s.Limit,
+				},
+				Time: s.TopPeriod,
+			}
+			posts, _, err = s.client.Subreddit.TopPosts(ctx, s.Subreddit, topOpts)
+		case "rising":
+			posts, _, err = s.client.Subreddit.RisingPosts(ctx, s.Subreddit, opts)
+		}
 	}
 
-	if s.RequestURLTemplate != "" {
-		requestURL = strings.ReplaceAll(s.RequestURLTemplate, "{REQUEST-URL}", requestURL)
-	} else if s.Proxy.client != nil {
-		client = s.Proxy.client
-	}
-
-	request, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
-		return nil, err
-	}
-	request.Header = headers
-
-	responseJson, err := decodeJsonFromRequest[subredditResponseJson](client, request)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("fetching posts: %v", err)
 	}
 
-	if len(responseJson.Data.Children) == 0 {
+	if len(posts) == 0 {
 		return nil, fmt.Errorf("no posts found")
 	}
 
-	posts := make(forumPostList, 0, len(responseJson.Data.Children))
-
-	for i := range responseJson.Data.Children {
-		post := &responseJson.Data.Children[i].Data
-
-		if post.Stickied || post.Pinned {
+	redditPosts := make([]*redditPost, 0, len(posts))
+	for _, post := range posts {
+		if post.Stickied {
 			continue
 		}
 
-		var commentsUrl string
-
-		if s.CommentsURLTemplate == "" {
-			commentsUrl = "https://www.reddit.com" + post.Permalink
-		} else {
-			commentsUrl = s.parseCustomCommentsURL(s.Subreddit, post.Id, post.Permalink)
-		}
-
-		forumPost := forumPost{
-			ID:              post.Id,
-			title:           html.UnescapeString(post.Title),
-			Description:     post.SelfText,
-			DiscussionUrl:   commentsUrl,
-			TargetUrlDomain: post.Domain,
-			CommentCount:    post.CommentsCount,
-			Score:           post.Upvotes,
-			TimePosted:      time.Unix(int64(post.Time), 0),
-		}
-
-		if post.Thumbnail != "" && post.Thumbnail != "self" && post.Thumbnail != "default" && post.Thumbnail != "nsfw" {
-			forumPost.ThumbnailUrl = html.UnescapeString(post.Thumbnail)
-		}
-
-		if !post.IsSelf {
-			forumPost.TargetUrl = post.Url
-		}
-
-		if s.ShowFlairs && post.Flair != "" {
-			forumPost.Tags = append(forumPost.Tags, post.Flair)
-		}
-
-		if len(post.ParentList) > 0 {
-			forumPost.IsCrosspost = true
-			forumPost.TargetUrlDomain = "r/" + post.ParentList[0].Subreddit
-
-			if s.CommentsURLTemplate == "" {
-				forumPost.TargetUrl = "https://www.reddit.com" + post.ParentList[0].Permalink
-			} else {
-				forumPost.TargetUrl = s.parseCustomCommentsURL(
-					post.ParentList[0].Subreddit,
-					post.ParentList[0].Id,
-					post.ParentList[0].Permalink,
-				)
-			}
-		}
-
-		if forumPost.TargetUrl != "" {
-			article, err := readability.FromURL(forumPost.TargetUrl, 5*time.Second)
-			if err == nil {
-				forumPost.Description += fmt.Sprintf("\n\nReferenced article: \n%s", article.TextContent)
-			} else {
-				slog.Error("Failed to fetch reddit article", "error", err, "url", forumPost.TargetUrl)
-			}
-		}
-
-		posts = append(posts, forumPost)
+		redditPosts = append(redditPosts, &redditPost{raw: post})
 	}
 
-	return posts, nil
-}
-
-func (s *redditSource) fetchNewAppAccessToken() error {
-	body := strings.NewReader("grant_type=client_credentials")
-	req, err := http.NewRequest("POST", "https://www.reddit.com/api/v1/access_token", body)
-	if err != nil {
-		return fmt.Errorf("creating request for app access token: %v", err)
-	}
-
-	app := &s.AppAuth
-	req.SetBasicAuth(app.ID, app.Secret)
-	req.Header.Add("User-Agent", app.Name+"/1.0")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	type tokenResponse struct {
-		AccessToken string `json:"access_token"`
-		ExpiresIn   int    `json:"expires_in"`
-	}
-
-	client := defaultHTTPClient
-	if s.Proxy.client != nil {
-		client = s.Proxy.client
-	}
-	response, err := decodeJsonFromRequest[tokenResponse](client, req)
-	if err != nil {
-		return err
-	}
-
-	app.accessToken = response.AccessToken
-	app.tokenExpiresAt = time.Now().Add(time.Duration(response.ExpiresIn) * time.Second)
-
-	return nil
+	return redditPosts, nil
 }
